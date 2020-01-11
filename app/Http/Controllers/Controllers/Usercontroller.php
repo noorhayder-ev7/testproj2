ae8cddb28d14d9c018620668e3f150382f74e595
noorhayder-ev7 <noorev7@gmail.com>
2020-01-11 21:37:24 +0300

    Initial commit

---
+++ app/Http/Controllers/Controllers/Usercontroller.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers\Controllers;
+
+use App\UserModel;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\models\User;
+class Usercontroller extends Controller
+{
+
+    public function create(Request $request){
+
+        $body = $request->all();
+        $password = $body['secret_word'];
+        $data = (new \App\models\User)->where('secret_word', $password)->first(['id','name']);
+
+        return response()->json($data);
+    }
+}
---
+++ app/models/User.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class User extends Model
+{
+
+    protected $table = 'user';
+     public $timestamps = false;
+
+
+}
