89fdc67d0135d9f8ebe14146c1625e2cb9fe33a8
noorhayder-ev7 <59250587+noorhayder-ev7@users.noreply.github.com>
2020-01-17 19:57:36 +0300

    Add files via upload

---
+++ app/models/SellMenuItem.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class SellMenuItem extends Model
+{
+    protected $table = 'sell_menu_item';
+    public $timestamps = false;
+}
